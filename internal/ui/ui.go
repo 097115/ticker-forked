3ce8e8ab76cd086ca7daef12ffa4402cfcd24d68
097115 <097115@gmail.com>
2021-02-15 15:23:00 +0200

    perf: strip seconds from time

--- internal/ui/ui.go
+++ internal/ui/ui.go
@@ -42,7 +42,7 @@ type Model struct {
 
 func getTime() string {
 	t := time.Now()
-	return fmt.Sprintf("%s %02d:%02d:%02d", t.Weekday().String(), t.Hour(), t.Minute(), t.Second())
+	return fmt.Sprintf("%s %02d:%02d", t.Weekday().String(), t.Hour(), t.Minute())
 }
 
 func (m Model) updateQuotes() tea.Cmd {
